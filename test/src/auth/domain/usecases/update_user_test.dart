8bba1cfb725a0bc1505b3489a303a9758276a5e5
andriySad <waterpolomen2@gmail.com>
2023-09-16 14:27:35 +0200

    minor refactor

--- test/src/auth/domain/usecases/update_user_test.dart
+++ test/src/auth/domain/usecases/update_user_test.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:tdd_courses_app/core/enums/update_user.dart';
 import 'package:tdd_courses_app/src/auth/domain/repos/auth_repo.dart';
 import 'package:tdd_courses_app/src/auth/domain/usecases/update_user.dart';
 
@@ -11,14 +10,13 @@ void main() {
   late IAuthRepo repository;
   late UpdateUser usecase;
 
+  const params = UpdateUserParams.empty();
   setUp(() {
     repository = MockAuthRepo();
     usecase = UpdateUser(repository);
 
-    registerFallbackValue(UpdateUserAction.empty);
+    registerFallbackValue(params.action);
   });
-
-  const params = UpdateUserParams.empty();
 
   test(
     'should call the [AuthRepo.updateUser]',
