0fc527dccceee377dfea763277b576e46cce2699
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-09-20 15:45:05 +0200

    added dashboard screen

---
+++ lib/src/dashboard/presentation/views/dashboard_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  static const routeName = '/dashboard';
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dashboard Screen'),
+    );
+  }
+}
